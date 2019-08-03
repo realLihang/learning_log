3fa4506210bc3952fdfc37987377495a76cfa3b8
Your NameLiHang <reallihang@163.com>
2019-08-03 10:09:19 +0800

    未完成上传

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -70,6 +70,7 @@ if os.getcwd() == '/app':
     # 静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'static'),
             )
